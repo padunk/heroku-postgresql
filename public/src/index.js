81c80172d1a80d14ba72cdf16dbfafcfcf0c1a2c
Padunk <abraham.agel@gmail.com>
2020-04-14 12:28:33 +0700

    second commit

---
+++ public/src/index.js
@@ -0,0 +1,2 @@
+'use strict'
+document.body.insertAdjacentHTML('beforeend', '<p>The source is index.js</p>')
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const path = require("path");
+const bp = require("body-parser");
 
 const app = express();
 
-app.listen(5000);
+app.use(express.static(path.join(__dirname, "public")));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
